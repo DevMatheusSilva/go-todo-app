0f6638fc4987024f97392b73e6625aec88ddc3f6
DevMatheusSilva <matheusmello225@gmail.com>
2024-11-16 17:28:46 -0300

    feat: integrate api with mongodb, and refactor routes

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,13 +1,38 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"github.com/DevMatheusSilva/go-todo-app/cmd/internal/config/db"
 	"github.com/DevMatheusSilva/go-todo-app/cmd/internal/routes"
 	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
 	"log"
+	"os"
 )
 
 func main() {
+	err := godotenv.Load("../internal/config/.env")
+	if err != nil {
+		log.Fatal("Error loading .env file:", err)
+	}
+
 	app := fiber.New()
-	routes.SetupTodoRoutes(app)
-	log.Fatal(app.Listen(":3000"))
+	ctx := context.Background()
+
+	client, database, err := db.Connect(ctx)
+	if err != nil {
+		fmt.Println("Error connecting to th database:", err)
+		return
+	}
+	defer client.Disconnect(ctx)
+
+	routes.SetupTodoRoutes(app, database)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+
+	log.Fatal(app.Listen("0.0.0.0:" + port))
 }
