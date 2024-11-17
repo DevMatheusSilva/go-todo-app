13fdfd14d0cb5d45a9fad70f97cc82cf1ca11fa7
DevMatheusSilva <matheusmello225@gmail.com>
2024-11-16 22:03:17 -0300

    refactor: refactor todo handlers to call specific repository methods

--- cmd/internal/handlers/todo_handlers.go
+++ cmd/internal/handlers/todo_handlers.go
@@ -1,60 +1,74 @@
 package handlers
 
 import (
-	"fmt"
+	"github.com/DevMatheusSilva/go-todo-app/cmd/internal/config/db"
 	"github.com/DevMatheusSilva/go-todo-app/cmd/internal/domain"
+	"github.com/DevMatheusSilva/go-todo-app/cmd/internal/repository"
 	"github.com/DevMatheusSilva/go-todo-app/cmd/internal/utils/consts"
 	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-var todos []domain.Todo
-
-func CreateTodo(c *fiber.Ctx) error {
-	todo := &domain.Todo{}
-
-	if err := c.BodyParser(todo); err != nil {
-		return err
-	}
-
-	if todo.Body == "" {
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": consts.ErrBodyRequired.Error()})
-	}
-
-	todo.ID = len(todos) + 1
-	todos = append(todos, *todo)
-
-	return c.Status(fiber.StatusCreated).JSON(todo)
-}
-
-func ShowAllTodos(c *fiber.Ctx) error {
-	if todos == nil || len(todos) == 0 {
-		return c.Status(fiber.StatusOK).JSON(fiber.Map{"message": consts.ErrNoTodosFound.Error()})
-	}
-	return c.Status(fiber.StatusOK).JSON(todos)
-}
-
-func CompleteTodo(c *fiber.Ctx) error {
-	id := c.Params("id")
-
-	for i, todo := range todos {
-		if fmt.Sprint(todo.ID) == id {
-			todos[i].Completed = !todos[i].Completed
-			return c.Status(fiber.StatusOK).JSON(todos[i])
+func CreateTodo(database *db.Database) fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		todo := new(domain.Todo)
+		if err := c.BodyParser(todo); err != nil {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
 		}
-	}
+		if todo.Body == "" {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": consts.ErrBodyRequired})
+		}
 
-	return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"error": consts.ErrTodoNotFound.Error()})
+		var savedTodo, err = repository.SaveTodo(c.Context(), database, todo)
+		if err != nil {
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": err.Error()})
+		}
+
+		return c.Status(fiber.StatusCreated).JSON(savedTodo)
+	}
 }
 
-func DeleteTodo(c *fiber.Ctx) error {
-	id := c.Params("id")
-
-	for i, todo := range todos {
-		if fmt.Sprint(todo.ID) == id {
-			todos = append(todos[:i], todos[i+1:]...)
-			return c.Status(fiber.StatusOK).JSON(fiber.Map{"message": consts.DeletedSuccessfullyMsg})
+func ShowAllTodos(database *db.Database) fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		var todos, err = repository.GetAllTodos(c.Context(), database)
+		if err != nil {
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": err.Error()})
 		}
-	}
 
-	return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"error": consts.ErrTodoNotFound.Error()})
+		return c.Status(fiber.StatusOK).JSON(todos)
+	}
+}
+
+func CompleteTodo(database *db.Database) fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		id := c.Params("id")
+		objectId, err := primitive.ObjectIDFromHex(id)
+		if err != nil {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": consts.ErrInvalidID})
+		}
+
+		err = repository.CompleteTodo(c.Context(), database, objectId)
+		if err != nil {
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": err.Error()})
+		}
+
+		return c.Status(fiber.StatusOK).JSON(fiber.Map{"message": consts.UpdatedSuccessfullyMsg})
+	}
+}
+
+func DeleteTodo(database *db.Database) fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		id := c.Params("id")
+		objectId, err := primitive.ObjectIDFromHex(id)
+		if err != nil {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": consts.ErrInvalidID})
+		}
+
+		err = repository.DeleteTodo(c.Context(), database, objectId)
+		if err != nil {
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": err.Error()})
+		}
+
+		return c.Status(fiber.StatusOK).JSON(fiber.Map{"message": consts.DeletedSuccessfullyMsg})
+	}
 }
